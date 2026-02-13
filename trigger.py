0764b311708a98f7ae5954198cfa04f87c8a063e
Barışcan Çeken <bariscanceken@hotmail.com>
2026-02-14 00:44:53 +0300

    trigger.py and ahtabot gif created.

---
+++ trigger.py
@@ -0,0 +1,50 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+from PyQt6.QtGui import QMovie
+from PyQt6.QtCore import Qt, QTimer
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+gif_path = os.path.join(base_dir, "media", "octosprite.gif")
+
+class Effect(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowFlags(
+            Qt.WindowType.FramelessWindowHint | 
+            Qt.WindowType.WindowStaysOnTopHint | 
+            Qt.WindowType.Tool
+        )
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+
+        self.label = QLabel(self)
+        self.movie = QMovie(gif_path)
+        self.label.setMovie(self.movie)
+        
+        self.movie.jumpToFrame(0) 
+        gifsize = self.movie.currentImage().size()
+        self.resize(gifsize)
+        self.label.setFixedSize(gifsize)
+        
+        self.movie.start()
+
+        screen = QApplication.primaryScreen().availableGeometry()
+        x = screen.width() - gifsize.width() - 20
+        y = screen.height() - gifsize.height() - 20 
+        self.move(x, y)
+
+        QTimer.singleShot(5000, self.close)
+
+    def closeEvent(self, event):
+
+        if hasattr(self, 'movie'):
+            self.movie.stop()
+        QApplication.quit()
+        event.accept()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    screen = Effect()
+    screen.show()
+    sys.exit(app.exec())
